805c8fa962483ab88d4821f71d1f6817cac208ea
Marko <kosicmarko95@gmail.com>
2023-07-25 15:17:44 +0200

    baseURL dodan u menu.js

--- blocks/menu.js
+++ blocks/menu.js
@@ -1,3 +1,5 @@
+const baseURL = 'https://cicin7.github.io/extra-frutta-2023';
+
 class menu extends HTMLElement {
   constructor() {
     super();
@@ -7,26 +9,26 @@ class menu extends HTMLElement {
 
       <!----- nav desk -----> 
       <div class="deskmenu">
-        <div class="logo"><img src="https://cicin7.github.io/extra-frutta-2023/img/extra-frutta-logo-transparent.png" alt="Extra Frutta logotip"></div>
+        <div class="logo"><img src="${baseURL}/img/extra-frutta-logo-transparent.png" alt="Extra Frutta logotip"></div>
         <ul class="deskwrap">
-          <li class="deskitem"> <a style="color:#0F5530;" href="./index.html"> Početna </a> </li>
+          <li class="deskitem"> <a style="color:#0F5530;" href="${baseURL}"> Početna </a> </li>
           <li class="menuicon menuicon-1"></li>
-          <li class="deskitem"> <a style="color:#0F5530;" href="./index.html#lista"> Lista proizvoda </a> </li>
+          <li class="deskitem"> <a style="color:#0F5530;" href="${baseURL}#lista"> Lista proizvoda </a> </li>
           <li class="menuicon menuicon-2"></li>
-          <li class="deskitem" id="listadesk"> <a style="color:#0F5530;" href="./index.html#tablica"> Voće i Povrće </a> </li>
+          <li class="deskitem" id="listadesk"> <a style="color:#0F5530;" href="${baseURL}#tablica"> Voće i Povrće </a> </li>
           <!-- 
           <li class="menuicon menuicon-3"></li>
-          <li class="deskitem"> <a style="color:#0F5530;" href="./index.html#kako"> Kako naručiti </a> </li>
+          <li class="deskitem"> <a style="color:#0F5530;" href="${baseURL}#kako"> Kako naručiti </a> </li>
           -->
           <li class="menuicon menuicon-4"></li>
-          <li class="deskitem"> <a style="color:#0F5530;" href="./index.html#kako"> Kontakt </a> </li>
+          <li class="deskitem"> <a style="color:#0F5530;" href="${baseURL}#kako"> Kontakt </a> </li>
           <li class="menuicon menuicon-5"></li>
-          <li class="deskitem"> <a style="color:#0F5530;" href="./index.html#onama"> O nama </a> </li>
+          <li class="deskitem"> <a style="color:#0F5530;" href="${baseURL}#onama"> O nama </a> </li>
         </ul>
       </div>
 
       <!----- nav mob -----> 
-      <div class="logomob"><img src="../../img/extra-frutta-logo-transparent.png" alt="Extra Frutta logotip"></div>
+      <div class="logomob"><img src="${baseURL}/img/extra-frutta-logo-transparent.png" alt="Extra Frutta logotip"></div>
       <div class="mobnav">
         <div class="barholder" id="mobtoggle">
           <div class="bar1"></div>
@@ -38,19 +40,19 @@ class menu extends HTMLElement {
       <!----- nav mob -----> 
       <div class="mobmenu" id="mobmenu">
         <ul class="mobwrap">
-          <li class="mobitem"> <a style="color:#fff;" href="./index.html"> Početna </a> </li>
+          <li class="mobitem"> <a style="color:#fff;" href="${baseURL}"> Početna </a> </li>
           <li class="menuicon menuicon-1"></li>
-          <li class="mobitem"> <a style="color:#fff;" href="./index.html#lista"> Lista proizvoda </a> </li>
+          <li class="mobitem"> <a style="color:#fff;" href="${baseURL}#lista"> Lista proizvoda </a> </li>
           <li class="menuicon menuicon-2"></li>
-          <li class="mobitem"> <a style="color:#fff;" href="./index.html#tablica"> Voće i Povrće </a> </li>
+          <li class="mobitem"> <a style="color:#fff;" href="${baseURL}#tablica"> Voće i Povrće </a> </li>
           <!-- 
           <li class="menuicon menuicon-3"></li>
-          <li class="mobitem"> <a style="color:#fff;" href="./index.html#kako"> Kako naručiti </a> </li>
+          <li class="mobitem"> <a style="color:#fff;" href="${baseURL}#kako"> Kako naručiti </a> </li>
           -->
           <li class="menuicon menuicon-4"></li>
-          <li class="mobitem"> <a style="color:#fff;" href="./index.html#kako"> Kontakt </a> </li>
+          <li class="mobitem"> <a style="color:#fff;" href="${baseURL}#kako"> Kontakt </a> </li>
           <li class="menuicon menuicon-5"></li>
-          <li class="mobitem"> <a style="color:#fff;" href="./index.html#onama"> O nama </a> </li>
+          <li class="mobitem"> <a style="color:#fff;" href="${baseURL}#onama"> O nama </a> </li>
         </ul>
       </div>
 
